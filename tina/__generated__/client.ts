3d0a60b150da4fb8c9628455e9be70d58b2424aa
ceciliabot1023 <131937305+ceciliabot1023@users.noreply.github.com>
2025-07-31 10:24:00 +0800

    tina update

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ cacheDir: '/Users/sudongyang/Desktop/trae_mcp/a-life-in-beta/tina/__generated__/.cache/1753928553732', url: 'https://content.tinajs.io/1.6/content/7456d39e-ac7f-4b47-ae65-83440dbde316/github/main', token: '32ed18c2ce49200920df2e38438e868d733004ff', queries,  });
 export default client;
   
--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -796,7 +796,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/1.6/content/7456d39e-ac7f-4b47-ae65-83440dbde316/github/main",
         queries,
       })
     )
