07f7d14e67b475f1e717109aef7f880464c66c32
ceciliabot1023 <131937305+ceciliabot1023@users.noreply.github.com>
2025-07-31 18:28:17 +0800

    suppress hydration warning

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/sudongyang/Desktop/trae_mcp/a-life-in-beta/tina/__generated__/.cache/1753956451145', url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
