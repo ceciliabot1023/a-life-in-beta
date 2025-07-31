5b9a22a73266d7235a277f317ace4e9ba3639d0c
ceciliabot1023 <131937305+ceciliabot1023@users.noreply.github.com>
2025-07-31 18:05:14 +0800

    revert to local mode

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/sudongyang/Desktop/trae_mcp/a-life-in-beta/tina/__generated__/.cache/1753955943128', url: 'http://localhost:4001/graphql', token: '896dd6592c32e64e5b68e82b5dab0f4cde56b85c', queries,  });
+export const client = createClient({ cacheDir: '/Users/sudongyang/Desktop/trae_mcp/a-life-in-beta/tina/__generated__/.cache/1753956290426', url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
