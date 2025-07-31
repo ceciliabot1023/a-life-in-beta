6562c205b6382c62deabb36858245a4bf6279b2c
ceciliabot1023 <131937305+ceciliabot1023@users.noreply.github.com>
2025-07-31 20:58:14 +0800

    env variable name update

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/sudongyang/Desktop/trae_mcp/a-life-in-beta/tina/__generated__/.cache/1753966446266', url: 'https://content.tinajs.io/1.6/content/190b6b72-2fd9-47d2-801b-43c84175955b/github/main', token: '896dd6592c32e64e5b68e82b5dab0f4cde56b85c', queries,  });
+export const client = createClient({ cacheDir: '/Users/sudongyang/Desktop/trae_mcp/a-life-in-beta/tina/__generated__/.cache/1753966605599', url: 'https://content.tinajs.io/content/undefined/github/undefined', token: '896dd6592c32e64e5b68e82b5dab0f4cde56b85c', queries,  });
 export default client;
   
